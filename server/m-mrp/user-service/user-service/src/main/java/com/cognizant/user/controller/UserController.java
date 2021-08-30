c75f75d590cb55c21489339db9c7bbb952686047
HarithaRajendran <haritharaj1811@gmail.com>
2021-08-31 00:42:13 +0530

    test

--- server/m-mrp/user-service/user-service/src/main/java/com/cognizant/user/controller/UserController.java
+++ server/m-mrp/user-service/user-service/src/main/java/com/cognizant/user/controller/UserController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -26,6 +27,7 @@ import com.cognizant.user.service.UserService;
 
 import lombok.extern.slf4j.Slf4j;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api/user")
 @Slf4j
